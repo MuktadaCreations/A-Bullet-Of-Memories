26ffeea5aba946ae8f323ff78c410722a4895c7f
MuktadaCreations <57871912+MuktadaCreations@users.noreply.github.com>
2020-05-31 03:49:25 -0700

    Add files via upload

---
+++ GLOBAL.gd
@@ -0,0 +1,42 @@
+extends Node
+
+#----------Player
+var Stamina = 100
+var Time = false
+var Ammo = 30
+var Health = 10
+#----------Player
+
+#----------Optins
+
+var Music = true
+var FX = true
+
+#----------Optins
+
+#----------Level Unlock
+var WallOne = false
+var WallTextOne = false
+var WallOneGoing = false
+
+var WallTwo = false
+var WallTextTwo = false
+var WallTwoGoing = false
+
+var WallThree = false
+var WallTextThree = false
+var WallThreeGoing = false
+#----------Level Unlock
+
+#----------Picture Unlock
+
+var PicPart1 = false
+var Picpart1Text = false
+
+var PicPart2 = false
+var Picpart2Text = false
+
+var PicPart3 = false
+var Picpart3Text = false
+
+#----------Picture Unlock
